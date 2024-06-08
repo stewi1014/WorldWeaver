ce8d376f0a3ceaa92cf99067e83fe02c049e967a
Frank Bauer <frank.bauer@fau.de>
2024-06-08 02:40:32 +0200

    Added missing files from recipe api

---
+++ wover-recipe-api/src/main/java/org/betterx/wover/entrypoint/LibWoverRecipe.java
@@ -0,0 +1,14 @@
+package org.betterx.wover.entrypoint;
+
+import org.betterx.wover.core.api.ModCore;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LibWoverRecipe implements ModInitializer {
+    public static final ModCore C = ModCore.create("wover-recipe", "wover");
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
