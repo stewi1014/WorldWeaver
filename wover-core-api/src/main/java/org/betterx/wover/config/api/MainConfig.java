68147a1209b090e0c0920664b066e4bf8f1bc821
Frank Bauer <frank.bauer@fau.de>
2024-06-15 12:33:35 +0200

    [Change] Auto add Sliders on `ConfigScreen` for Float or Integer values

--- wover-core-api/src/main/java/org/betterx/wover/config/api/MainConfig.java
+++ wover-core-api/src/main/java/org/betterx/wover/config/api/MainConfig.java
@@ -8,6 +8,8 @@ public class MainConfig extends ConfigFile {
     public final static Group WORLD_LOADING = new Group(LibWoverCore.C.namespace, "loading", 800);
     public final static Group SERVER_GROUP = new Group(LibWoverCore.C.namespace, "server", 1000);
     public final static Group STRUCTURE_GROUP = new Group(LibWoverCore.C.namespace, "structure", 2000);
+    public final static Group PERFORMANCE_GROUP = new Group(LibWoverCore.C.namespace, "performance", 3000);
+    public final static Group COSMETIC_GROUP = new Group(LibWoverCore.C.namespace, "cosmetic", 4000);
 
     public final static String LOG_CATEGORY = "log";
     public final static String SERVER_CATEGORY = SERVER_GROUP.title();
