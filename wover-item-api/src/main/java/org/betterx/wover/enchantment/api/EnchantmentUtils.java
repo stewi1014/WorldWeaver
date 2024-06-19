126a9159cac40c0c22917eea9ead8f2dd5eb21ca
Frank Bauer <frank.bauer@fau.de>
2024-06-19 23:14:59 +0200

    [Feature] Some useful Enchantment Utilities

--- wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentUtils.java
+++ wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentUtils.java
@@ -1,13 +1,51 @@
 package org.betterx.wover.enchantment.api;
 
+import net.minecraft.core.Holder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.level.Level;
 
 public class EnchantmentUtils {
     private EnchantmentUtils() {
+    }
+
+    public static Holder<Enchantment> getEnchantment(Level world, ResourceKey<Enchantment> enchantment) {
+        return world
+                .registryAccess()
+                .registry(Registries.ENCHANTMENT)
+                .flatMap(r -> r.getHolder(enchantment))
+                .orElse(null);
+    }
+
+    public static int getItemEnchantmentLevel(
+            Level world,
+            ResourceKey<Enchantment> enchantment,
+            ItemStack stack
+    ) {
+        final Holder<Enchantment> enc = getEnchantment(world, enchantment);
+        if (enc != null) {
+            return EnchantmentHelper.getItemEnchantmentLevel(enc, stack);
+        }
+
+        return 0;
+    }
+
+    public static int getItemEnchantmentLevel(
+            Level world,
+            ResourceKey<Enchantment> enchantment,
+            LivingEntity entity
+    ) {
+        final Holder<Enchantment> enc = getEnchantment(world, enchantment);
+        if (enc != null) {
+            return EnchantmentHelper.getEnchantmentLevel(enc, entity);
+        }
+
+        return 0;
     }
 
     /**
