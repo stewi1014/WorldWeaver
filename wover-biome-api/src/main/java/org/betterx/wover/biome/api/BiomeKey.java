c1cbc7b515d4aa8e131a340c0d981dbe259f926f
Frank Bauer <frank.bauer@fau.de>
2024-06-19 23:16:23 +0200

    [Feature] `BiomeManager` offers convenience Methods to lookup the `BiomeData` for a given Biome.

--- wover-biome-api/src/main/java/org/betterx/wover/biome/api/BiomeKey.java
+++ wover-biome-api/src/main/java/org/betterx/wover/biome/api/BiomeKey.java
@@ -6,11 +6,18 @@ import org.betterx.wover.biome.api.data.BiomeData;
 import org.betterx.wover.biome.api.data.BiomeDataRegistry;
 import org.betterx.wover.biome.impl.BiomeManagerImpl;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistryAccess;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public abstract class BiomeKey<B extends BiomeBuilder<B>> {
     /**
@@ -24,6 +31,59 @@ public abstract class BiomeKey<B extends BiomeBuilder<B>> {
      */
     @NotNull
     public final ResourceKey<BiomeData> dataKey;
+
+    /**
+     * Gets the {@link Holder} for the {@link Biome} from the given getter.
+     *
+     * @param getter The getter to get the holder from or {@code null}
+     * @return The holder for the {@link Biome} or {@code null} if it is not present
+     */
+    @Nullable
+    public Holder<Biome> getHolder(@Nullable HolderGetter<Biome> getter) {
+        return getter == null ? null : getter.get(this.key).orElse(null);
+    }
+
+    /**
+     * Gets the {@link Holder} for the {@link Biome} from the given getter.
+     *
+     * @param access The registry access to get the holder from
+     * @return The holder for the {@link Biome} or {@code null} if it is not present
+     */
+    @Nullable
+    public Holder<Biome> getHolder(@Nullable RegistryAccess access) {
+        return access == null
+                ? null
+                : access.registryOrThrow(Registries.BIOME).getHolder(this.key).orElse(null);
+    }
+
+    /**
+     * Gets the {@link Holder} for the {@link Biome} from the given getter.
+     *
+     * @param provider The provider to get the holder from or {@code null}
+     * @return The holder for the {@link Biome} or {@code null} if it is not present
+     */
+    @Nullable
+    public Holder<Biome> getHolder(@Nullable HolderLookup.@NotNull Provider provider) {
+        return provider == null
+                ? null
+                : provider.lookup(Registries.BIOME).orElseThrow().get(this.key).orElse(null);
+    }
+
+
+    /**
+     * Gets the {@link Holder} for the {@link Biome} from the given getter.
+     *
+     * <p>
+     * This method internally looks up {@link Registries#BIOME}. If you need to retrieve
+     * a lot of holders, it is recommended to manually lookup the
+     * Registry first and use {@link #getHolder(HolderGetter)} instead.
+     *
+     * @param context The {@link BootstrapContext} to get the holder from
+     * @return The holder for the {@link Biome} or {@code null} if it is not present
+     */
+    public Holder<Biome> getHolder(@NotNull BootstrapContext<?> context) {
+        return context.lookup(Registries.BIOME).get(this.key).orElse(null);
+    }
 
     public abstract B bootstrap(BiomeBootstrapContext context);
 
--- wover-biome-api/src/main/java/org/betterx/wover/biome/api/BiomeManager.java
+++ wover-biome-api/src/main/java/org/betterx/wover/biome/api/BiomeManager.java
@@ -2,15 +2,22 @@ package org.betterx.wover.biome.api;
 
 import org.betterx.wover.biome.api.builder.BiomeBuilder;
 import org.betterx.wover.biome.api.builder.event.OnBootstrapBiomes;
+import org.betterx.wover.biome.api.data.BiomeData;
+import org.betterx.wover.biome.api.data.BiomeDataRegistry;
 import org.betterx.wover.biome.impl.BiomeManagerImpl;
 import org.betterx.wover.events.api.Event;
 import org.betterx.wover.events.api.types.OnBootstrapRegistry;
+import org.betterx.wover.state.api.WorldState;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.RegistryAccess;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class BiomeManager {
     public static final Event<OnBootstrapRegistry<Biome>> BOOTSTRAP_BIOME_REGISTRY
@@ -24,5 +31,35 @@ public class BiomeManager {
 
     public static BiomeKey<BiomeBuilder.Wrapped> wrapped(@NotNull ResourceKey<Biome> key) {
         return BiomeManagerImpl.wrapped(key);
+    }
+
+    /**
+     * Get {@link BiomeData} for biome. This Method will use the {@link WorldState} to get the
+     * {@link BiomeDataRegistry} and then get the {@link BiomeData} for the given biome.
+     *
+     * @param biome - {@link Holder<Biome>} from.
+     * @return {@link BiomeData} or null if it was not found.
+     */
+    public static @Nullable BiomeData biomeDataForHolder(Holder<Biome> biome) {
+        return biomeDataForHolder(WorldState.registryAccess(), biome);
+    }
+
+    /**
+     * Get {@link BiomeData} for biome. This Method will use the {@link RegistryAccess} to get the
+     * {@link BiomeDataRegistry} and then get the {@link BiomeData} for the given biome.
+     *
+     * @param acc   The RegistryAccess
+     * @param biome - {@link Holder<Biome>} from.
+     * @return {@link BiomeData} or null if it was not found.
+     */
+    public static @Nullable BiomeData biomeDataForHolder(RegistryAccess acc, Holder<Biome> biome) {
+        if (acc != null) {
+            final Registry<BiomeData> reg = acc.registryOrThrow(BiomeDataRegistry.BIOME_DATA_REGISTRY);
+            ResourceLocation id = biome.unwrapKey().map(ResourceKey::location).orElse(null);
+            if (id != null) {
+                return reg.get(id);
+            }
+        }
+        return null;
     }
 }
