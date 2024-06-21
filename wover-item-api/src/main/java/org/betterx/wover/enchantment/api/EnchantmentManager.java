1c0fdad39c6c87aa0e45bd011d216b16844a63c7
Frank Bauer <frank.bauer@fau.de>
2024-06-22 00:39:52 +0200

    [Change] Added Method to register Enchantment Attributes

--- wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentManager.java
+++ wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentManager.java
@@ -5,9 +5,13 @@ import org.betterx.wover.enchantment.impl.EnchantmentManagerImpl;
 import org.betterx.wover.events.api.Event;
 import org.betterx.wover.events.api.types.OnBootstrapRegistry;
 
+import net.minecraft.core.Holder;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.item.enchantment.Enchantment;
 
 public class EnchantmentManager {
@@ -15,5 +19,9 @@ public class EnchantmentManager {
 
     public static EnchantmentKey createKey(ResourceLocation id) {
         return new EnchantmentKeyImpl(ResourceKey.create(Registries.ENCHANTMENT, id));
+    }
+
+    public static Holder<Attribute> registerAttribute(ResourceLocation string, Attribute attribute) {
+        return Registry.registerForHolder(BuiltInRegistries.ATTRIBUTE, string, attribute);
     }
 }
