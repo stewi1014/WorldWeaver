f081658cf88f39bed9b8bcab5e30e10ba6ecd2ea
Frank Bauer <frank.bauer@fau.de>
2024-06-22 13:49:22 +0200

    [Change] Method to register DataComponents

--- wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentManager.java
+++ wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentManager.java
@@ -7,12 +7,15 @@ import org.betterx.wover.events.api.types.OnBootstrapRegistry;
 
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.item.enchantment.Enchantment;
+
+import java.util.function.UnaryOperator;
 
 public class EnchantmentManager {
     public static final Event<OnBootstrapRegistry<Enchantment>> BOOTSTRAP_ENCHANTMENTS = EnchantmentManagerImpl.BOOTSTRAP_ENCHANTMENTS;
@@ -23,5 +26,16 @@ public class EnchantmentManager {
 
     public static Holder<Attribute> registerAttribute(ResourceLocation string, Attribute attribute) {
         return Registry.registerForHolder(BuiltInRegistries.ATTRIBUTE, string, attribute);
+    }
+
+    public static <T> DataComponentType<T> registerDataComponent(
+            ResourceLocation id,
+            UnaryOperator<DataComponentType.Builder<T>> builder
+    ) {
+        return Registry.register(
+                BuiltInRegistries.DATA_COMPONENT_TYPE,
+                id,
+                builder.apply(DataComponentType.builder()).build()
+        );
     }
 }
