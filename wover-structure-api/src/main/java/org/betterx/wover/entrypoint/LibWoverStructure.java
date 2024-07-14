08497e53c9d3c3f54e06fdae4af8ccd0777f6974
Frank Bauer <frank.bauer@fau.de>
2024-07-14 14:26:15 +0200

    [Fix] Make sure `RANDOM_NBT_STRUCTURE_TYPE` is initialized as early as possible.

--- wover-structure-api/src/main/java/org/betterx/wover/entrypoint/LibWoverStructure.java
+++ wover-structure-api/src/main/java/org/betterx/wover/entrypoint/LibWoverStructure.java
@@ -1,6 +1,7 @@
 package org.betterx.wover.entrypoint;
 
 import org.betterx.wover.core.api.ModCore;
+import org.betterx.wover.structure.impl.StructureManagerImpl;
 import org.betterx.wover.structure.impl.pools.StructurePoolElementTypeManagerImpl;
 
 import net.fabricmc.api.ModInitializer;
@@ -12,7 +13,7 @@ public class LibWoverStructure implements ModInitializer {
     public void onInitialize() {
         StructurePoolElementTypeManagerImpl.ensureStaticallyLoaded();
         //StructurePoolManagerImpl.initialize(); //done in the wover.datapack.registry entrypoint
-        //StructureManagerImpl.initialize(); //done in the wover.datapack.registry entrypoint
+        StructureManagerImpl.initialize();
         //StructureSetManagerImpl.initialize(); //done in the wover.datapack.registry entrypoint
     }
 }
