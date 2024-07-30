d493540a3a1c42ebd30db1062b3beb1d32df33ed
Frank Bauer <frank.bauer@fau.de>
2024-07-30 17:58:54 +0200

    [Feature] API to register EffectComponents

--- wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentManager.java
+++ wover-item-api/src/main/java/org/betterx/wover/enchantment/api/EnchantmentManager.java
@@ -5,14 +5,12 @@ import org.betterx.wover.enchantment.impl.EnchantmentManagerImpl;
 import org.betterx.wover.events.api.Event;
 import org.betterx.wover.events.api.types.OnBootstrapRegistry;
 
-import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.component.DataComponentType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.item.enchantment.Enchantment;
 
 import java.util.function.UnaryOperator;
@@ -24,18 +22,12 @@ public class EnchantmentManager {
         return new EnchantmentKeyImpl(ResourceKey.create(Registries.ENCHANTMENT, id));
     }
 
-    public static Holder<Attribute> registerAttribute(ResourceLocation string, Attribute attribute) {
-        return Registry.registerForHolder(BuiltInRegistries.ATTRIBUTE, string, attribute);
-    }
-
-    public static <T> DataComponentType<T> registerDataComponent(
+    public static <T> DataComponentType<T> registerEffectComponent(
             ResourceLocation id,
-            UnaryOperator<DataComponentType.Builder<T>> builder
+            UnaryOperator<DataComponentType.Builder<T>> unaryOperator
     ) {
-        return Registry.register(
-                BuiltInRegistries.DATA_COMPONENT_TYPE,
-                id,
-                builder.apply(DataComponentType.builder()).build()
-        );
+        return Registry.register(BuiltInRegistries.ENCHANTMENT_EFFECT_COMPONENT_TYPE, id, unaryOperator
+                .apply(DataComponentType.builder())
+                .build());
     }
 }
