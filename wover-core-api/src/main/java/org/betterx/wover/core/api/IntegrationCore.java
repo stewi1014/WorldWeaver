7014d06022ebea6a0df5460fca0b8caecd32ffaf
Frank Bauer <frank.bauer@fau.de>
2024-08-10 22:38:03 +0200

    [Change] Get vanilla Namespace from constant

--- wover-core-api/src/main/java/org/betterx/wover/core/api/IntegrationCore.java
+++ wover-core-api/src/main/java/org/betterx/wover/core/api/IntegrationCore.java
@@ -1,5 +1,7 @@
 package org.betterx.wover.core.api;
 
+import net.minecraft.resources.ResourceLocation;
+
 import net.fabricmc.loader.api.FabricLoader;
 
 public class IntegrationCore {
@@ -12,7 +14,7 @@ public class IntegrationCore {
     public static final boolean RUNS_TERRABLENDER = hasMod("terrablender");
     public static final boolean RUNS_NULLSCAPE = hasMod("nullscape");
 
-    public static final ModCore MINECRAFT = ModCore.create("minecraft");
+    public static final ModCore MINECRAFT = ModCore.create(ResourceLocation.DEFAULT_NAMESPACE);
     public static final ModCore BETTER_END = ModCore.create("betterend");
     public static final ModCore BETTER_NETHER = ModCore.create("betternether");
 }
